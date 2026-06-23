44b90867f8371843f6d9dcb63480894a4f1b4c39
Samarth <samarthunde2003@gmail.com>
2026-06-24 01:53:18 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="Medical_Assistant",
+    version="0.0.1",
+    author="Samarth Unde",
+    author_email="samarthunde2003@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
