4629b43ce9bf30c37a395301f40b3369353edf32
Samarth <samarthunde2003@gmail.com>
2026-06-25 01:00:56 +0530

    update

---
+++ src/helper.py
@@ -0,0 +1,33 @@
+from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+
+
+# Extract text from PDF files in the "data" directory
+def load_pdf_file(data):
+    loader = DirectoryLoader(data,
+                             glob="*.pdf",
+                             loader_cls=PyPDFLoader
+                             )
+    
+    documents = loader.load()
+    
+    return documents
+    
+    
+# split the text into smaller chunks for processing
+def text_split(extracted_data):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=20
+    )
+    
+    text_chunks = text_splitter.split_documents(extracted_data)
+    
+    return text_chunks
+
+
+# download the embeddings from Hugging Face
+def download_hugging_face_embeddings():
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    return embeddings
---
+++ src/prompt.py
@@ -0,0 +1,9 @@
+system_prompt = (
+    "You are an assistant for question-answering task."
+    "Use the following piece of retirival context to answer the question at the end."
+    "If you don't know the answer, just say you don't know. Don't try to make up an answer."
+    "Use three sentences maximum. and  keep the answer as concise as possible."
+    "\n\n"
+    "{context}"
+    
+)
