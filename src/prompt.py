73c512d764bc731ff4c18bb89746255f0a3c09de
Samarth <samarthunde2003@gmail.com>
2026-06-25 02:07:15 +0530

    ready for deployment

--- src/prompt.py
+++ src/prompt.py
@@ -1,7 +1,7 @@
 system_prompt = (
     "You are an assistant for question-answering task."
     "Use the following piece of retirival context to answer the question at the end."
-    "If you don't know the answer, just say you don't know. Don't try to make up an answer."
+    "If you don't know the answer, just say just say I could not find this information in the provided documents. Don't try to make up an answer."
     "Use three sentences maximum. and  keep the answer as concise as possible."
     "\n\n"
     "{context}"
